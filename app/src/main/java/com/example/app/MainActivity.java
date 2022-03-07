c9aa7c9206c803f5703e5e0e9754a2015b6cd9c9
yue <1263887632@qq.com>
2022-03-07 23:23:24 +0800

    feat: update h5

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.app;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.JsPromptResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private WebView mWebView;
+    JsPromptResult mJsPromptResult;
 
     @Override
     @SuppressLint("SetJavaScriptEnabled")
@@ -16,23 +25,64 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = findViewById(R.id.activity_main_webview);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mJsPromptResult != null) {
+                    mJsPromptResult.confirm("wtf");
+                }
+            }
+        });
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+                Log.d("yue_", "message: " + message);
+                Log.d("yue_", "defaultValue: " + defaultValue);
+                Log.d("yue_", "result: " + result);
+                Log.d("yue_", "thread: " + Thread.currentThread());
+                mJsPromptResult = result;
+                if (!TextUtils.isEmpty(message)) {
+                    result.confirm("hello world");
+                    return true;
+                }
+                return super.onJsPrompt(view, url, message, defaultValue, result);
+            }
 
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("yue_", "consoleMessage: " + consoleMessage.message());
+                return super.onConsoleMessage(consoleMessage);
+            }
+
+        });
         // REMOTE RESOURCE
         // mWebView.loadUrl("https://example.com");
 
         // LOCAL RESOURCE
-        // mWebView.loadUrl("file:///android_asset/index.html");
+        mWebView.loadUrl("file:///android_asset/index.html");
     }
 
     @Override
     public void onBackPressed() {
-        if(mWebView.canGoBack()) {
+        if (mWebView.canGoBack()) {
             mWebView.goBack();
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mWebView.evaluateJavascript("javascript:alertTest()", new ValueCallback<String>() {
+            @Override
+            public void onReceiveValue(String value) {
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
